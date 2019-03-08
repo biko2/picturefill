a26a3f42635b43de2b10a196322c8e6b86a98f51
David Gallego Varela <david.gallego@biko2.com>
2019-03-08 12:42:03 +0100

    Check if window exists

--- src/plugins/gecko-picture/pf.gecko-picture.js
+++ src/plugins/gecko-picture/pf.gecko-picture.js
@@ -4,65 +4,81 @@
  * not react to viewport changes. This tiny module fixes this.
  */
 (function(window) {
-	/*jshint eqnull:true */
-	var ua = navigator.userAgent;
+    if (!window) {
+        return;
+    }
+    /*jshint eqnull:true */
+    var ua = navigator.userAgent;
 
-	if ( window.HTMLPictureElement && ((/ecko/).test(ua) && ua.match(/rv\:(\d+)/) && RegExp.$1 < 45) ) {
-		addEventListener("resize", (function() {
-			var timer;
+    if (
+        window.HTMLPictureElement &&
+        (/ecko/.test(ua) && ua.match(/rv\:(\d+)/) && RegExp.$1 < 45)
+    ) {
+        addEventListener(
+            "resize",
+            (function() {
+                var timer;
 
-			var dummySrc = document.createElement("source");
+                var dummySrc = document.createElement("source");
 
-			var fixRespimg = function(img) {
-				var source, sizes;
-				var picture = img.parentNode;
+                var fixRespimg = function(img) {
+                    var source, sizes;
+                    var picture = img.parentNode;
 
-				if (picture.nodeName.toUpperCase() === "PICTURE") {
-					source = dummySrc.cloneNode();
+                    if (picture.nodeName.toUpperCase() === "PICTURE") {
+                        source = dummySrc.cloneNode();
 
-					picture.insertBefore(source, picture.firstElementChild);
-					setTimeout(function() {
-						picture.removeChild(source);
-					});
-				} else if (!img._pfLastSize || img.offsetWidth > img._pfLastSize) {
-					img._pfLastSize = img.offsetWidth;
-					sizes = img.sizes;
-					img.sizes += ",100vw";
-					setTimeout(function() {
-						img.sizes = sizes;
-					});
-				}
-			};
+                        picture.insertBefore(source, picture.firstElementChild);
+                        setTimeout(function() {
+                            picture.removeChild(source);
+                        });
+                    } else if (
+                        !img._pfLastSize ||
+                        img.offsetWidth > img._pfLastSize
+                    ) {
+                        img._pfLastSize = img.offsetWidth;
+                        sizes = img.sizes;
+                        img.sizes += ",100vw";
+                        setTimeout(function() {
+                            img.sizes = sizes;
+                        });
+                    }
+                };
 
-			var findPictureImgs = function() {
-				var i;
-				var imgs = document.querySelectorAll("picture > img, img[srcset][sizes]");
-				for (i = 0; i < imgs.length; i++) {
-					fixRespimg(imgs[i]);
-				}
-			};
-			var onResize = function() {
-				clearTimeout(timer);
-				timer = setTimeout(findPictureImgs, 99);
-			};
-			var mq = window.matchMedia && matchMedia("(orientation: landscape)");
-			var init = function() {
-				onResize();
+                var findPictureImgs = function() {
+                    var i;
+                    var imgs = document.querySelectorAll(
+                        "picture > img, img[srcset][sizes]"
+                    );
+                    for (i = 0; i < imgs.length; i++) {
+                        fixRespimg(imgs[i]);
+                    }
+                };
+                var onResize = function() {
+                    clearTimeout(timer);
+                    timer = setTimeout(findPictureImgs, 99);
+                };
+                var mq =
+                    window.matchMedia && matchMedia("(orientation: landscape)");
+                var init = function() {
+                    onResize();
 
-				if (mq && mq.addListener) {
-					mq.addListener(onResize);
-				}
-			};
+                    if (mq && mq.addListener) {
+                        mq.addListener(onResize);
+                    }
+                };
 
-			dummySrc.srcset = "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
+                dummySrc.srcset =
+                    "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
 
-			if (/^[c|i]|d$/.test(document.readyState || "")) {
-				init();
-			} else {
-				document.addEventListener("DOMContentLoaded", init);
-			}
+                if (/^[c|i]|d$/.test(document.readyState || "")) {
+                    init();
+                } else {
+                    document.addEventListener("DOMContentLoaded", init);
+                }
 
-			return onResize;
-		})());
-	}
-})(window);
+                return onResize;
+            })()
+        );
+    }
+})(typeof window === "undefined" ? null : window);
